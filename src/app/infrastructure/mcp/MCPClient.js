427e9a02e3fb4850ce27511dd59740819d657618
Pucho <martinezochandarenam@gmail.com>
2026-08-13 13:16:57 +0200

    feat: create mcp client

---
+++ src/app/infrastructure/mcp/MCPClient.js
@@ -0,0 +1,43 @@
+import {
+  Client,
+//   type AuthProvider,
+} from "@modelcontextprotocol/sdk/client/index.js";
+import { StreamableHTTPClientTransport } from "@modelcontextprotocol/sdk/client/streamableHttp.js";
+
+import { Auth0TokenProvider } from "../auth/Auth0TokenProvider.js";
+
+export class MCPClient {
+
+  constructor( tokenProvider  ) {
+    this.client = new Client({ name: "next-agent", version: "1.0.0" });
+    this.tokenProvider = tokenProvider || new Auth0TokenProvider();
+  }
+
+  async connect() {
+
+    const transport = new StreamableHTTPClientTransport(
+      new URL(
+        "https://sampling-mcp-production.up.railway.app/mcp",
+      ),
+      {
+        fetch: async (url, init) => {
+          const token = await this.tokenProvider.getAccessToken();
+
+          const headers = new Headers(init?.headers);
+          headers.set("Authorization", `Bearer ${token}`);
+
+          return fetch(url, { ...init, headers });
+        },
+      },
+    );
+
+    await this.client.connect(transport);
+    console.log('Connected successfully.');
+
+
+     const tools = await this.client.listTools();
+    console.log('Available tools:', tools.tools.map(t => t.name).join(', ') || '(none)');
+
+    await transport.close();
+  }
+}
